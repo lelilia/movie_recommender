522a34219fa3383bb71b16f9cf45701851c08daa
miriamino <29210881+miriamino@users.noreply.github.com>
2020-12-09 14:55:54 +0100

    even more css

--- forms.py
+++ forms.py
@@ -7,4 +7,4 @@ class InitialMovieRatingForm(FlaskForm):
     rating_best = RadioField(choices=[1,2,3,4,5], default=5)
     movie_worst = StringField('Worst Movie', validators=[DataRequired()], render_kw={"placeholder": "your least favorite movie", "class": "form-control form-control-sm"})
     rating_worst = RadioField(choices=[1,2,3,4,5], default=1)
-    submit = SubmitField('Recommend')
+    submit = SubmitField('get recommendation', render_kw={"class": "btn btn-primary"})

e4497ab78127750c8137057938bf62dbbbb6fb1e
miriamino <29210881+miriamino@users.noreply.github.com>
2020-12-09 14:33:19 +0100

    more css

--- forms.py
+++ forms.py
@@ -3,8 +3,8 @@ from wtforms import StringField, RadioField, SubmitField
 from wtforms.validators import DataRequired
 
 class InitialMovieRatingForm(FlaskForm):
-    movie_best = StringField('Best Movie', validators=[DataRequired()])
+    movie_best = StringField('Best Movie', validators=[DataRequired()], render_kw={"placeholder": "your favorite movie", "class": "form-control form-control-sm"})
     rating_best = RadioField(choices=[1,2,3,4,5], default=5)
-    movie_worst = StringField('Worst Movie', validators=[DataRequired()])
+    movie_worst = StringField('Worst Movie', validators=[DataRequired()], render_kw={"placeholder": "your least favorite movie", "class": "form-control form-control-sm"})
     rating_worst = RadioField(choices=[1,2,3,4,5], default=1)
     submit = SubmitField('Recommend')

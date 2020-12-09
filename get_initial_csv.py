684bf221db12685089c1296c0af0e845096b79ad
Leli <lelilia@posteo.de>
2020-12-09 17:14:21 +0100

    adding init

--- get_initial_csv.py
+++
@@ -1,7 +0,0 @@
-''' Saves the data in a csv with userId as index and movieId as columns '''
-
-import pandas as pd
-
-ratings = pd.read_csv('data/ratings.csv')
-ratings = ratings.pivot(index='userId', columns='movieId', values='rating')
-ratings.to_csv('data/initial_list.csv')
